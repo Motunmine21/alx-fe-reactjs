5839727d01fc6a7214706bc85f5fcbfc364d24c1
Motunmine21 <216323922+Motunmine21@users.noreply.github.com>
2026-01-24 04:11:20 -0800

    Update the App.jsx file with the new content

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,23 +1,21 @@
 
 import React from 'react';
 import UserProfile from './components/UserProfile';
-
+import './App.css';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 function App() {
   return (
     <div>
-      <h1>My React App</h1>
-
-      {/* Using the UserProfile component */}
-      <UserProfile 
+       <Header />
+       <MainContent />
+       <Footer />
+        <h1>My React App</h1>
+        <UserProfile 
         name="Alice" 
         age={25} 
         bio="Loves hiking and photography" 
-      />
-
-      <UserProfile 
-        name="Bob" 
-        age={30} 
-        bio="Enjoys painting and traveling" 
       />
     </div>
   );
