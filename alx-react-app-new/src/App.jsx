1275d86949445bb3feb8df590869bbf26cc0cf6b
Motunmine21 <216323922+Motunmine21@users.noreply.github.com>
2026-01-24 05:21:40 -0800

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import UserProfile from './components/UserProfile';
+import './App.css';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <div>
+       <Header />
+       <MainContent />
+       <Footer />
+        <h1>My React App</h1>
+        <UserProfile 
+        name="Alice" 
+        age={25} 
+        bio="Loves hiking and photography" 
+      />
+    </div>
+  );
+}
+
+export default App;
+
