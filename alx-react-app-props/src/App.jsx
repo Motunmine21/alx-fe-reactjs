b9597b504f99855186506171c64ff4e18ad95ee6
Motunmine21 <216323922+Motunmine21@users.noreply.github.com>
2026-01-25 03:19:08 -0800

    Refactor Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,33 @@
+
+import UserContext from "./context/UserContext";
+import ProfilePage from "./components/ProfilePage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import "./App.css";
+function App() {
+  const userData = {
+    name: "Alice",
+    age: 25,
+    bio: "Loves hiking and photography",
+  }
+  return (
+    
+    <UserContext.Provider value={userData}>
+      <Header />
+      <MainContent />
+      <h1>My React App</h1>
+      <ProfilePage />
+      <Footer />
+    </UserContext.Provider>
+  );
+}
+
+  
+
+ 
+   
+  
+
+export default App;
+
