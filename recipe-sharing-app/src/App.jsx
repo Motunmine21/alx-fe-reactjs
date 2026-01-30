afc5a4fa946dacd8e8c38fb52b85df65c31fbaee
Motunmine21 <216323922+Motunmine21@users.noreply.github.com>
2026-01-30 01:54:36 -0800

    Initialize the Recipe Sharing Application with Zustand State Management

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,17 @@
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+import './App.css'
+function App() {
+  return (
+    <div>
+      <h1>Recipe App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
+
+
+
