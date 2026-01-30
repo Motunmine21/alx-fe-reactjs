918770b05ffa2b0ca987ea517aa7e730aa22436e
Motunmine21 <216323922+Motunmine21@users.noreply.github.com>
2026-01-30 02:41:22 -0800

    Update the DeleteRecipeButton file

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,9 +1,14 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useRecipeStore } from "./recipeStore";
 
 const DeleteRecipeButton = () => {
   const navigate = useNavigate();
+  const { id } = useParams();
+
+  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
   const handleDelete = () => {
+    deleteRecipe(Number(id));
     navigate("/");
   };
 
