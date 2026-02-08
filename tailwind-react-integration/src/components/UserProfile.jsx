21f38aa91ff6328e16925bc32e4931639b6a0e5c
Motunmine21 <216323922+Motunmine21@users.noreply.github.com>
2026-02-08 13:41:48 -0800

    Add Advanced Interactivity and Transitions with Tailwind CSS'

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,6 +8,10 @@ function UserProfile() {
         mx-auto my-20
         rounded-lg
         shadow-lg
+        hover:shadow-xl
+        transition-shadow
+        duration-300
+        ease-in-out
         text-center
       "
     >
@@ -19,6 +23,10 @@ function UserProfile() {
           md:w-36 md:h-36
           rounded-full
           mx-auto
+          transition-transform
+          duration-300
+          ease-in-out
+          hover:scale-110
         "
       />
 
@@ -27,6 +35,10 @@ function UserProfile() {
           sm:text-lg md:text-xl
           text-blue-800
           my-4
+          transition-colors
+          duration-300
+          ease-in-out
+          hover:text-blue-500
         "
       >
         John Doe
